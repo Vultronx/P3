8ba1cfe2843774422c98243ebca9aaf16a766507
Vultronx <142888084+Vultronx@users.noreply.github.com>
2023-09-21 20:08:01 +0200

    Ajout de commentaires
    Ajout de commentaires pour l'implémentation de la barre de filtre

--- FrontEnd/get.js
+++ FrontEnd/get.js
@@ -14,6 +14,12 @@ async function get() {
         addWork(galleryClass, worksJson[i])
         i++;
     }
+
+    /************************ Récupérer les catégories et mettre en place la barre de filtre ************************/
+    //dans le JSON les projets sont classés par catégories identifiables par un ID dans la propriété "categoryId"
+    //faire un fetch sur l'adresse http://localhost:5678/api/categories afin de récupérer les catégories classées par ID
+    //j'affiche ensuite les catégories sous forme de boutons filtres. Exemple : | Tous | categoryId | categoryId | categoryId | ... |
+    //j'affiche ensuite les projets de la catégorie filtrée
 };
 
 //function permettant l'ajout d'un projet dans la galerie
