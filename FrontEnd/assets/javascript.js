c0fe3ecb5eb210aa6544085b84b77957172eca46
Vultronx <142888084+Vultronx@users.noreply.github.com>
2023-09-23 10:08:16 +0200

    Update HTML, CSS and JS

--- FrontEnd/assets/javascript.js
+++ FrontEnd/assets/javascript.js
@@ -1,7 +1,7 @@
 //Récupération des projets et affichage dans le DOM
 //le paramètre attendu est un entier correspondant à l'ID de la catégorie souhaitée
 //par defaut, categorieID = 0. Affichage de tous les projets si aucune valeur n'a été renseignée
-async function get(categorieID = 0) {
+async function appendCategoriesAndWorks(categorieID = 0) {
     //récupération des travaux sur le serveur
     const worksResponse = await fetch("http://localhost:5678/api/works");
     const worksJson = await worksResponse.json();
@@ -10,47 +10,76 @@ async function get(categorieID = 0) {
     const categoriesResponse = await fetch("http://localhost:5678/api/categories");
     const categoriesJson = await categoriesResponse.json();
 
-    //Affichage temporaire des catégories dans la console
-    console.log(categoriesJson);
+    //reinitialisation des catégories
+    const categoriesClass = document.querySelector(".categories");
+    categoriesClass.innerHTML = "";
 
+    //ajout des catégories dans la barre de filtre
+    appendCategorie(categoriesClass, {id: 0, name: 'Tous'});
+    let i = 0;
+    while (i < categoriesJson.length) {
+        appendCategorie(categoriesClass, categoriesJson[i]);
+        i++;
+    };
+    
     //reinitialisation de la galerie
     const galleryClass = document.querySelector(".gallery");
     galleryClass.innerHTML = "";
 
-    //boucle permettant l'ajout des projets du JSON dans la galerie
-    let i = 0;
+    //ajout des projets dans la galerie
+    i = 0;
     while (i < worksJson.length) {
         if (worksJson[i].category.id == categorieID || categorieID == 0) {
-            addWork(galleryClass, worksJson[i]);
+            appendWork(galleryClass, worksJson[i]);
         }
         i++;
     };
 
     /************************ Récupérer les catégories et mettre en place la barre de filtre ************************/
-    //dans le JSON les projets sont classés par catégories identifiables par un ID dans la propriété "categoryId"
+    //dans le JSON, les projets sont classés par catégories identifiables par un ID dans la propriété "categoryId"
     //faire un fetch sur l'adresse http://localhost:5678/api/categories afin de récupérer les catégories classées par ID
     //j'affiche ensuite les catégories sous forme de boutons filtres. Exemple : | Tous | categoryId | categoryId | categoryId | ... |
     //j'affiche ensuite les projets de la catégorie filtrée
 };
 
 //function permettant l'ajout d'un projet dans la galerie
-function addWork(parentClass, workJson) {
+function appendWork(gallery, work) {
     //déclaration des éléments à ajouter dans la galerie
     let figureElement = document.createElement("figure");
     let imageElement = document.createElement("img");
     let figcaptionElement = document.createElement("figcaption");
 
     //définition du contenu des éléments
-    imageElement.src = workJson.imageUrl;
-    imageElement.alt = workJson.title;
-    figcaptionElement.innerHTML = workJson.title;
+    imageElement.src = work.imageUrl;
+    imageElement.alt = work.title;
+    figcaptionElement.innerHTML = work.title;
 
     //ajout des éléments image et figcaption dans l'élément figure
     figureElement.appendChild(imageElement);
     figureElement.appendChild(figcaptionElement);
 
     //ajout des éléments dans la galerie
-    parentClass.appendChild(figureElement);
+    gallery.appendChild(figureElement);
 };
 
-get();
+//function permettant l'ajout d'une catégorie dans la barre de filtre
+function appendCategorie(filterbar, categorie) {
+    //déclaration des éléments à ajouter dans la barre de filtre
+    let pElement = document.createElement("p");
+    let divElement = document.createElement("div");
+
+    //définition du contenu des éléments
+    pElement.innerHTML = categorie.name;
+    divElement.className = "button";
+    divElement.addEventListener("click",  () => {
+        appendCategoriesAndWorks(categorie.id);
+    });
+
+    //ajout de l'élément p dans l'élément div
+    divElement.appendChild(pElement);
+    
+    //ajout des éléments dans la barre de filtre
+    filterbar.appendChild(divElement);
+};
+
+appendCategoriesAndWorks();
