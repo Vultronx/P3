90b7727caf0c18f118489c8ad3bcc4fc651e08bf
Vultronx <142888084+Vultronx@users.noreply.github.com>
2023-10-18 13:22:47 +0200

    Update

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,3 +1,79 @@
+(async function main() {
+
+    let root = this;
+
+    const WORKS_URL = "http://localhost:5678/api/works";
+    const CATEGORIES_URL = "http://localhost:5678/api/categories";
+    const LOGIN_URL = "http://localhost:5678/api/users/login"
+
+    this.Works_JSON = null;
+    this.Categories_JSON = null;
+
+    this.works = {
+        url: "http://localhost:5678/api/works",
+        json: null,
+        init: async function () {
+            console.log("init...");
+            let result = await this.update();
+            if (result)
+                console.log("ready.");
+        },
+        update: async function () {
+            console.log("update...");
+            //récupération des catégories sur le serveur    
+            let response = await fetch(this.url);
+            if (!response.ok)
+                return false;
+            this.json = await response.json();
+            return true;
+        },
+        append: function (worksJson, categoryId = 0) {
+            appendWorks(worksJson, categoryId);
+        }
+    };
+
+    this.categories = {
+        url: "http://localhost:5678/api/works",
+        json: null,
+        init: async function () {
+            console.log("init...");
+            let result = await this.update();
+            if (result)
+                console.log("ready.");
+        },
+        update: async function () {
+            console.log("update...");
+            //récupération des catégories sur le serveur    
+            let response = await fetch(this.url);
+            if (!response.ok)
+                return false;
+            this.json = await response.json();
+            return true;
+        },
+        append: function (filterbar) {
+            appendCategories(filterbar);
+        }
+    };
+
+    this.login = {
+        init: function () {
+
+        },
+        start: function () {
+
+        }
+    };
+
+    /*console.log("Works start ...");
+    await this.works.init();
+    console.log("Categories start ...");
+    await this.categories.init();
+    console.log("Script ready.");
+    console.log("Works_JSON : "+this.works.json);
+    console.log("Categories_JSON : "+this.categories.json);*/
+
+})();
+
 //Récupération des projets et affichage dans le DOM
 //le paramètre attendu est un entier correspondant à l'ID de la catégorie souhaitée
 //par defaut, categorieID = 0. Affichage de tous les projets si aucune valeur n'a été renseignée
@@ -5,11 +81,8 @@
 
     //racine du script
     const root = this;
-
-    //projets au format JSON
     this.Works_JSON = null;
-    //catégories au format JSON
-    this.Category_JSON = null;
+    this.Categories_JSON = null;
 
     //Récupération des catégories sur le serveur    
     const CATEGORIES_RESPONSE = await fetch("http://localhost:5678/api/categories");
@@ -23,7 +96,8 @@
     const filterbar = document.querySelector(".filterbar");
     this.worksManageModal = document.getElementById("works-manage");
     this.worksAddModal = document.getElementById("works-add");
-    
+
+    this.workPicture = document.querySelector("#works-add #work-picture");    
 
     //ajout des catégories dans la barre de filtre
     appendCategories(filterbar);
@@ -69,11 +143,14 @@
         let pElement = document.createElement("p");
         let divElement = document.createElement("div");
 
+        let categoriesSelectElement = document.getElementById("work-category");
+        let optionElement = document.createElement("option");
+
         //définition du contenu des éléments
         pElement.innerHTML = category.name;
         divElement.className = "button";
         divElement.addEventListener("click",  () => {
-            appendWorks(this.Works_JSON, category.id);
+            appendWorks(WORKS_JSON, category.id);
         });
 
         //ajout de l'élément p dans l'élément div
@@ -81,6 +158,12 @@
         
         //ajout des éléments dans la barre des catégories
         filterbar.appendChild(divElement);
+
+        if (category.name != "Tous") {
+            optionElement.value = category.name;
+            optionElement.innerHTML = category.name;
+            categoriesSelectElement.appendChild(optionElement);
+        }
     };
 
     //function permettant l'ajout d'un projet dans la galerie
@@ -135,43 +218,132 @@
             const addButtonElement = document.querySelector(".works-manage .add-button");
             const previousButtonElement = document.querySelector(".works-add .previous-button");
             const closeButtonElement = document.querySelector(".works-add .close-button");
+
+            const pictureElement = document.querySelector(".picture");
+            const addImageButtonElement = document.querySelector(".add-image-button");
+            const pElement = document.querySelector(".picture-overview p");
+
+            let oldFileName = "";
+            let imageElement = document.createElement("img");
+
             addButtonElement.addEventListener("click", () => {
                 root.worksManageModal.style.display = "none";
                 root.worksAddModal.style.display = "block";
             });
             previousButtonElement.addEventListener("click", () => {
+                const pictureOverviewElement = document.querySelector(".picture-overview");
+
                 root.worksManageModal.style.display = "block";
                 root.worksAddModal.style.display = "none";
+
+                imageElement.style.display = "none";
+                pictureElement.style.display = "flex";
+                addImageButtonElement.style.display = "flex";
+                pElement.style.display = "flex";
+                pictureOverviewElement.style.padding = "16px 0px 20px 0px";
+                pictureOverviewElement.style.height = "142px";
             });
             closeButtonElement.addEventListener("click", () => {
+                const pictureOverviewElement = document.querySelector(".picture-overview");
+
                 //fermeture de la modale par appel aux nodes parents
                 closeButtonElement.parentNode.parentNode.style.display = "none";
+                
+                imageElement.style.display = "none";
+                pictureElement.style.display = "flex";
+                addImageButtonElement.style.display = "flex";
+                pElement.style.display = "flex";
+                pictureOverviewElement.style.padding = "16px 0px 20px 0px";
+                pictureOverviewElement.style.height = "142px";
+            });
+            window.onclick = function(event) {
+                if (event.target == this.worksManageModal) {
+                    root.worksManageModal.style.display = "none";
+                } else if (event.target == this.worksAddModal) {
+                    root.worksAddModal.style.display = "none";
+
+                    const pictureOverviewElement = document.querySelector(".picture-overview");
+                    
+                    root.workPicture.style.display = "none";
+                    pictureElement.style.display = "flex";
+                    addImageButtonElement.style.display = "flex";
+                    pElement.style.display = "flex";
+                    pictureOverviewElement.style.padding = "16px 0px 20px 0px";
+                    pictureOverviewElement.style.height = "142px";
+                }
+            }
+            //Gestion de l'input qui permet de récupérer l'image d'un nouveau projet à ajouter
+            const pictureInputElement = document.getElementById("picture-input");
+            pictureInputElement.addEventListener("change", (input) => {
+                console.log("Name : "+pictureInputElement.files[0].name);
+                console.log("Size : "+pictureInputElement.files[0].size);
+                console.log(window.URL.createObjectURL(pictureInputElement.files[0]));
+                const pictureOverviewElement = document.querySelector(".picture-overview");
+
+                console.log("testA : "+(root.workPicture.src));
+                root.workPicture.src = window.URL.createObjectURL(pictureInputElement.files[0]);
+                /*root.workPicture.onload = function () {
+                    //window.URL.revokeObjectURL(this.src);
+                    oldFileName = this.src;
+                };*/
+                console.log(root.workPicture.src);
+
+                pictureElement.style.display = "none";
+                addImageButtonElement.style.display = "none";
+                pElement.style.display = "none";
+
+                root.workPicture.style.display = "block";
+                root.workPicture.style.width = "auto";
+                root.workPicture.style.height = "100%";
+
+                pictureOverviewElement.style.padding = "0px 0px 0px 0px";
+                pictureOverviewElement.style.height = "178px";
+                pictureOverviewElement.appendChild(root.workPicture);
+                
+                pictureInputElement.value = "";
+
             });
             this.update();
         },
+        manage: function () {
+
+        },
         add: function () {
 
-        },        
+        },
         remove: function () {
 
         },
         update: function () {
-            Works_JSON = this.get();
+            root.Works_JSON = this.get();
         },
         get: async function () {
             //récupération des travaux sur le serveur    
             let json = await fetch("http://localhost:5678/api/works")
             .then((response) => {
                 if (!response.ok)
-                    console.log("Erreur lors de la récupération des travaux")
+                    console.log("Erreur lors de la récupération des données")
                     return false;
             })
             return json;
         }
+    }.init();
+
+    this.categories = {
+        init: async function () {
+            await this.update();
+        },
+        update: async function () {
+            //récupération des catégories sur le serveur    
+            let response = await fetch("http://localhost:5678/api/categories");
+            if (!response.ok)
+                return false;
+            root.Categories_JSON = await response.json();
+            return true;
+        }
     };
-    
-    //initialisation des projets
-    this.works.init();
+
+    await root.categories.init();
 
     this.menu = {
         init: function () {
@@ -241,6 +413,7 @@
         }
     };
     this.menu.init();
+    this.menu.editorShow();
 
     this.login = {
         init: function () {
@@ -335,10 +508,42 @@
             const worksModifyButton = document.querySelector(".works-modify-button");
             worksModifyButton.addEventListener("click", function () {
                 root.worksManageModal.style.display = "block";
-            })
+            });
             const worksModifyCloseButton = document.querySelector(".works-manage .close-button");
             worksModifyCloseButton.addEventListener("click", function () {
                 root.worksManageModal.style.display = "none";
+            });
+            const pictureInputElement = document.getElementById("picture-input");
+            const worksSendButton = document.querySelector(".works-add .send-button");
+            
+            let workPicture = document.querySelector("#works-add #work-picture");
+            let workTitle = document.querySelector("#works-add #work-title");
+            let workCategory = document.querySelector("#works-add #work-category");
+            worksSendButton.addEventListener("click", function () {
+                //fetch et actualisation de la liste des projet
+                //here
+                
+                //récupération des valeurs du formulaire pour l'ajout d'un projet
+                console.log("url : "+workPicture.src);
+                console.log("title : "+workTitle.value);
+                console.log("id : "+workCategory.selectedIndex);
+
+                // Appel de la fonction fetch avec toutes les informations nécessaires
+                fetch("http://localhost:5678/api/works", {
+                    method: "POST",
+                    headers: {
+                        Accept: "application/json",
+                        "Content-Type": "multipart/form-data",
+                        "Authorization": "Bearer "+localStorage.getItem("token"),
+                    },
+                    body: JSON.stringify({
+                        imageUrl: workPicture.src,
+                        title: workTitle.value,
+                        category: workCategory.selectedIndex,
+                    }),
+                });
+                pictureInputElement.value = "";
+                root.worksAddModal.style.display = "none";
             })
         },
         open: function () {
@@ -351,6 +556,10 @@
     this.editor.init();
 
 })();
+
+//initialisation des catégories
+//sbScript.categories.init();
+
 //const colis = await fetch("http://api.top-livraisons.fr/colis").then(colis => colis.json());
 //const livreur = await fetch("http://api.top-livreurs.fr/livreurs/" + colis.livreurId).then(livreur => livreur.json());
 /*fetch("/colis/livraison", {
